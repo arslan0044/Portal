d751d66029da88331b1a95a9c8112e14cac4de4f
arslan0044 <arslan.saqib121@gmail.com>
2023-09-23 13:12:48 +0500

    Update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from api import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    # path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls'))
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 from api import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('',include(urls))
 ]
